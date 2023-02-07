cb6d39f928019c77d17cc644a654d42b8a47c09e
Artur Upton Renault <artur@niftory.com>
2023-02-07 13:22:37 -0800

    Add another dep

--- packages/service/index.ts
+++ packages/service/index.ts
@@ -1,6 +1,8 @@
 import { getGoogle } from "localPackage";
+import { v4 } from "uuid";
 
 export const handler = async () => {
+  console.log(v4());
   return {
     statusCode: 200,
     body: getGoogle(),
